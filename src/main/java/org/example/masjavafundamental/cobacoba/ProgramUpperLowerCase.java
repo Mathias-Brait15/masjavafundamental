908863d770715acce77f896ce3f52de442e35ccd
Mathias-Brait15 <61750659+Mathias-Brait15@users.noreply.github.com>
2023-07-03 21:25:35 +0700

    Cobacoba

---
+++ src/main/java/org/example/masjavafundamental/cobacoba/ProgramUpperLowerCase.java
@@ -0,0 +1,18 @@
+package org.example.masjavafundamental.cobacoba;
+
+public class ProgramUpperLowerCase {
+    public static void main(String[] args) {
+
+    }
+}
+
+/*
+IntelliJ IDEA 2022.3.1 (Community Edition)
+Build #IC-223.8214.52, built on December 20, 2022
+@Author Lenovo a.k.a. Paul Christian
+Java Developer
+Created on 03/07/2023 21:23
+@Last Modified 03/07/2023 21:23
+Version 1.0
+*/
+
